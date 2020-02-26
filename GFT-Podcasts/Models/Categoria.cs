482169b2cf9e657d6b99947cc7de934d2782a8dc
Vinícius Araújo <44041869+ViniciusHSAraujo@users.noreply.github.com>
2020-02-26 11:36:13 -0300

    Criação das Models

---
+++ GFT-Podcasts/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace GFT_Podcasts.Models {
+    public class Categoria {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public virtual IEnumerable<Podcast> Podcasts { get; set; }
+        
+    }
+}
---
+++ GFT-Podcasts/Models/Episodio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GFT_Podcasts.Models {
+    public class Episodio {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Descricao { get; set; }
+
+        public DateTime Lancamento { get; set; }
+
+        public double Duracao { get; set; }
+
+        public string LinkAudio { get; set; }
+
+        public int PodcastId { get; set; }
+        [ForeignKey(nameof(PodcastId))] public virtual Podcast Podcast { get; set; }
+    }
+}
